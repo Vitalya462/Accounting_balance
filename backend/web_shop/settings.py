ba57121a215e4e886578b85dcf8c3df2549d5f43
TheAppleKingy <pridanov.ali.job@gmail.com>
2025-05-12 16:10:39 +0700

    turn off csrf

--- backend/web_shop/settings.py
+++ backend/web_shop/settings.py
@@ -122,7 +122,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'auth.authentications.CsrfExemptSessionAuthentication',
     ]
 }
 
